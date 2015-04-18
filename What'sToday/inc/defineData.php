6000fcf7ca991cbf85e96b9916dd243c9a62909b
fumio <fumio@172.16.33.1>
2015-04-18 16:24:27 +0900

    一旦commit

---
+++ What'sToday/inc/defineData.php
@@ -0,0 +1,9 @@
+<?php
+	
+	// データベース用設定データ
+	define('DATABASE_HOST','localhost');
+	define('DATABASE_NAME','whatsToday');
+	define('DATABASE_USER','root');
+	define('DATABASE_PASSWORD','fumio1226');
+	
+	define('PDO_DSN','mysql:dbname=' . DATABASE_NAME .';host=' . DATABASE_HOST);
--- What'sToday/readXMLData.php
+++ What'sToday/readXMLData.php
@@ -1,15 +1,21 @@
 <?php
 	date_default_timezone_set("Asia/Tokyo");
 	
+	require_once 'inc/defineData.php';
+	
 	global $argc;
 	global $argv;
 	
 	$fileDateName = $argv[1] . "月" . $argv[2] . "日.xml";
 	
-	$loadXML = simplexml_load_file($fileDateName);
+	$loadXML = simplexml_load_file('sampleData/' . $fileDateName);
 	
-	print($loadXML);
+	var_dump($loadXML);
 	
+	// データベース接続
+	$pdo = new PDO( PDO_DSN, DATABASE_USER, DATABASE_PASSWORD );
 	
-	
+	$insertDataSQL = "INSERT INTO todayData SET (day, cat_id, hash, dayTitle, dayDetail) 
+			VALUES (:day, :cat_id, :hash, :dayTitle, :dayDetail)";
+	$stmt = $pdo->prepare($insertDataSQL);
 	
